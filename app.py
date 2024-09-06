b8c00f538c9cdea4782121e6be6a3591bd27bb58
willsco <williamgambas@gmail.com>
2024-09-05 23:24:48 -0300

    feat: poo - call new controller on route 'countdown'

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from controllers.countdown_controller import countdown
+from controllers.CountdownController import CountdownController
 
 app = Flask(__name__)
 
-app.add_url_rule('/countdown', 'countdown', countdown)
+countdown_controller = CountdownController()
+
+app.add_url_rule('/countdown', 'countdown', countdown_controller.countdown)
 
 if __name__ == "__main__":
     app.run(debug=True)
